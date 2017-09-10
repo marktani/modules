e6f23b893365e7c8d2a45819f1e8faa5c5c753e9
Søren Bramer Schmidt <sorenbs@gmail.com>
2017-09-10 16:40:24 +0200

    Algolia sync Module

---
+++ syncing/algolia/code/sync.js
@@ -0,0 +1,40 @@
+const algoliasearch = require('algoliasearch');
+
+// replace with your own values
+// var client = algoliasearch('YourApplicationID', 'YourAPIKey');
+// var index = client.initIndex('yourIndexName');
+// const modelName = 'yourModelName'
+
+module.exports = function (event) {
+  
+  const mutation = event.data[modelName].mutation
+  const node = event.data[modelName].node
+  const previousValues = event.data[modelName].previousValues
+
+  switch (mutation) {
+    case "CREATED":
+    return syncAddedNode(node)
+    case "UPDATED":
+    return syncUpdatedNode(node)
+    case "DELETED":
+    return syncDeletedNode(previousValues)
+    default:
+    console.log(`mutation was '${mutation}'. Unable to sync node.`)
+    return Promise.resolve()
+  }
+}
+
+function syncAddedNode(node) {
+  console.log('Adding node')
+  return index.addObject(node) 
+}
+
+function syncUpdatedNode(node) {
+  console.log('Updating node')
+  return index.saveObject(node) 
+}
+
+function syncDeletedNode(node) {
+  console.log('Deleting node')
+  return index.deleteObject(node.objectId)
+}
---
+++ syncing/algolia/readme.md
@@ -0,0 +1,11 @@
+# algolia
+
+Add automatic Algolia indexing to your Graphcool project ⚡️
+
+## Getting Started
+
+```sh
+npm -g install graphcool
+graphcool init
+graphcool module add graphcool/modules/syncing/algolia
+```
